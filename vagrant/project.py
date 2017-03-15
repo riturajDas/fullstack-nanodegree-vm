245e1cbe8d72c43874c05c01ce1106bacc8d103e
Rituraj Das <rituraj.das8@gmail.com>
2017-03-15 14:45:17 +0530

    Added anchors to link to the create and delete page

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -11,19 +11,10 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
-def HelloWorld(restaurant_id):
+def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    output = ''
-    for i in items:
-        output += i.name
-        output += '<br>'
-        output += i.price
-        output += '<br>'
-        output += i.description
-        output += '<br>'
-        output += '<br>'
-    return output
+    return render_template('menu.html', restaurant = restaurant, items = items)
 
 #Task1 : create new items
 @app.route('/restaurant/<int:restaurant_id>/create/')
