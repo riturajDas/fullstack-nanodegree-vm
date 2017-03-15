1599b53d6d91721c6af53d8f2249c85143371623
Rituraj Das <rituraj.das8@gmail.com>
2017-03-15 15:43:28 +0530

    added DB linkup to create a new item for a restaurant

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -10,16 +10,22 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/restaurants/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     return render_template('menu.html', restaurant = restaurant, items = items)
 
 #Task1 : create new items
-@app.route('/restaurant/<int:restaurant_id>/create/')
-def createMenuItem(restaurant_id):
-    return 'Page to create new menu item for %s ' % restaurant_id
+@app.route('/restaurant/<int:restaurant_id>/new/', methods = ['GET', 'POST'])
+def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('newMenuItem.html', restaurant_id = restaurant_id)
 
 #Task2 : edit menu items
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
